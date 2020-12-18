751e9c12c15fe506b1fc0d9d0cfc887dfd7eb10a
Jack Coggins <voidawake90@gmail.com>
2020-12-18 14:40:23 +1100

    Add painter

---
+++ Assets/Scripts/Painter.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Painter : MonoBehaviour
+{
+    [SerializeField] private Color color;
+
+    void Start()
+    {
+        GetComponent<SpriteRenderer>().color = color;
+    }
+
+    void OnTriggerEnter2D (Collider2D col) {
+        if (col.tag == "Egg") {
+            col.GetComponent<Egg>().Paint(color);
+        }
+    }
+}
