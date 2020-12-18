3a371c8d531cf4795df5d19fc3005cf9fa9340da
Jack Coggins <voidawake90@gmail.com>
2020-12-18 15:40:34 +1100

    Add basket

---
+++ Assets/Scripts/Basket.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Basket : MonoBehaviour
+{
+    [SerializeField] private int buildIndex;
+
+    private void OnTriggerEnter2D (Collider2D col) {
+        if (col.tag == "Egg") {
+            SceneManager.LoadScene(buildIndex);
+        }
+    }
+}
