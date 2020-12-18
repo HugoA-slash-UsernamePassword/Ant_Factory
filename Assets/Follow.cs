38b11ede1eb7327bb276efe3a0c438cdd4efd158
HugoA-slash-UsernamePassword <hugoa3520@gmail.com>
2020-12-18 19:02:54 +1100

    stuff 3

--- Assets/Follow.cs
+++ Assets/Follow.cs
@@ -14,8 +14,17 @@ public class Follow : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (editor.egg) transform.position = Vector3.Lerp(transform.position, editor.egg.transform.position, .25f);
-        else transform.position = Vector3.Lerp(transform.position, new Vector3((Screen.width-Input.mousePosition.x)/Screen.width*56-6,(Screen.height-Input.mousePosition.y)/Screen.height*100-50) , 0.05f);
+        if (editor.egg)
+        {
+            transform.position = Vector3.Lerp(transform.position, editor.egg.transform.position, .25f);
+            Camera.main.orthographicSize = Mathf.Lerp(Camera.main.orthographicSize, 8, 0.15f);
+        }
+        else
+        {
+            transform.position = Vector3.Lerp(transform.position, new Vector2(25, 5), .25f);
+            Camera.main.orthographicSize = Mathf.Lerp(Camera.main.orthographicSize, 24, 0.15f);
+        }
+        //else transform.position = Vector3.Lerp(transform.position, new Vector3((Screen.width-Input.mousePosition.x)/Screen.width*56-6,(Screen.height-Input.mousePosition.y)/Screen.height*75-25) , 0.05f);
         transform.position = new Vector3(Mathf.Clamp(transform.position.x, -6, 50), Mathf.Clamp(transform.position.y, -50, 50), -10);
     }
 }
