33e032b4d81a61e0ae6f312d3eb4e3eea590832a
Jack Coggins <voidawake90@gmail.com>
2020-12-18 14:34:58 +1100

    Add bumper

---
+++ Assets/Scripts/Bumper.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bumper : MonoBehaviour
+{
+    private Animator animator;
+
+    private void Start () {
+        animator = GetComponent<Animator>();
+    }
+
+    void OnCollisionEnter2D (Collision2D col) {
+        if (col.gameObject.tag == "Egg") {
+            Bounce();
+        }
+    }
+
+    void Bounce () {
+        animator.SetTrigger("Bounce");
+    }
+}
