2b1b899a414931bc6d1e290857ad6d0719f01d96
Jack Coggins <voidawake90@gmail.com>
2020-12-18 16:27:52 +1100

    Add cannon rotation

---
+++ Assets/Scripts/ArrowRotate.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrowRotate : MonoBehaviour
+{
+    [SerializeField] private float rotateSpeed;
+
+    void Update()
+    {
+        
+
+        transform.Rotate(0, 0, Input.GetAxis("Horizontal") * -1);
+    }
+}
