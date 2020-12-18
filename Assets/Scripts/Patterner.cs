eb052f45d58b2972617182292a84f5f2c05ee1ac
Jack Coggins <voidawake90@gmail.com>
2020-12-18 15:57:33 +1100

    Update patterner to display pattern

--- Assets/Scripts/Patterner.cs
+++ Assets/Scripts/Patterner.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -8,17 +9,40 @@ public class Patterner : MonoBehaviour
 
     private Animator animator;
 
+    [SerializeField] GameObject layerPrefab;
+    [SerializeField] Transform layerSpawnPoint;
+    [SerializeField] Sprite nonePattern;
+    [SerializeField] Sprite spotsPattern;
+    [SerializeField] Sprite stripesPattern;
+    [SerializeField] Sprite specklesPattern;
+
     void Start()
     {
-        // *** Sprite needs to reflect pattern
         animator = GetComponent<Animator>();
+
+        GameObject layerObject = Instantiate(layerPrefab, layerSpawnPoint.position, layerSpawnPoint.rotation, transform);
+
+        SpriteRenderer layerSpriteRend = layerObject.GetComponent<SpriteRenderer>();
+        
+        layerSpriteRend.sprite = PatternToSprite(pattern);
+        layerSpriteRend.color = new Color(1, 1, 1, 0.5f);
     }
 
     void OnTriggerEnter2D (Collider2D col) {
         if (col.tag == "Egg") {
             animator.SetTrigger("Stamp");
-            // Should this also hold the egg while it is stamped
+
             col.GetComponent<Egg>().ApplyPattern(pattern);
+        }
+    }
+
+    private Sprite PatternToSprite (Egg.Pattern pattern) {
+        switch (pattern) {
+            case Egg.Pattern.None: return nonePattern;
+            case Egg.Pattern.Spots: return spotsPattern;
+            case Egg.Pattern.Stripes: return stripesPattern;
+            case Egg.Pattern.Speckles: return specklesPattern;
+            default: throw new ArgumentOutOfRangeException();
         }
     }
 }
