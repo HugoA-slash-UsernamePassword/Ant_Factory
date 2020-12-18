060a23fa027fac1b391e8c3b5baac4f45ed4a3d7
Jack Coggins <voidawake90@gmail.com>
2020-12-18 14:41:48 +1100

    Add patterner

---
+++ Assets/Scripts/Patterner.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Patterner : MonoBehaviour
+{
+    [SerializeField] private Egg.Pattern pattern;
+
+    private Animator animator;
+
+    void Start()
+    {
+        // *** Sprite needs to reflect pattern
+        animator = GetComponent<Animator>();
+    }
+
+    void OnTriggerEnter2D (Collider2D col) {
+        if (col.tag == "Egg") {
+            animator.SetTrigger("Stamp");
+            // Should this also hold the egg while it is stamped
+            col.GetComponent<Egg>().ApplyPattern(pattern);
+        }
+    }
+}
